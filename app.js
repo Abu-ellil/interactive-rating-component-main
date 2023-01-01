75b799338ab762d1ddf7087d4df0e5e3c7b0b7a0
Abo Ellil <mr.abuellil@gmail.com>
2023-01-01 04:02:27 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const links = document.getElementById([])
+
+const selectedEl = document.getElementById('1');
+const btn = document.getElementById('btn')
+console.log(selectedEl);
+
+selectedEl .addEventListener('click',function(){
+  selectedEl.classList.toggle('selected')
+})
+()
